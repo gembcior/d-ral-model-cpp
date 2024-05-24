af4390a61dec9d701ba407f4547e7edf9541be48
Gembcior <gembcior@gmail.com>
2024-05-24 22:11:35 +0200

    Dockerfile. Compilation. First app.

---
+++ cmake/toolchain/armv7m.cmake
@@ -0,0 +1,25 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+find_path(GNU_ARM_EMBEDDED_TOOLCHAIN
+  NAMES
+    "arm-none-eabi-gcc"
+    "arm-none-eabi-g++"
+  DOC "GNU Arm Embedded Toolchain"
+  PATHS
+    /usr/lib/arm-none-eabi
+)
+
+set(CMAKE_C_COMPILER "${GNU_ARM_EMBEDDED_TOOLCHAIN}/arm-none-eabi-gcc")
+set(CMAKE_CXX_COMPILER "${GNU_ARM_EMBEDDED_TOOLCHAIN}/arm-none-eabi-g++")
+set(CMAKE_ASM_COMPILER "${GNU_ARM_EMBEDDED_TOOLCHAIN}/arm-none-eabi-gcc")
+set(CMAKE_AR "${GNU_ARM_EMBEDDED_TOOLCHAIN}/arm-none-eabi-ar")
+set(CMAKE_RANLIB "${GNU_ARM_EMBEDDED_TOOLCHAIN}/arm-none-eabi-ranlib")
+
+# See https://cmake.org/cmake/help/latest/manual/cmake-toolchains.7.html
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
---
+++ cmake/toolchain/stm32f429.cmake
@@ -0,0 +1,35 @@
+include(${CMAKE_CURRENT_LIST_DIR}/armv7m.cmake)
+
+string(CONCAT MCU_FLAGS
+  "-mcpu=cortex-m4 "
+  "-mfpu=fpv4-sp-d16 "
+  "-mfloat-abi=hard "
+  "-mabi=aapcs "
+  "-mthumb "
+)
+
+string(CONCAT C_FLAGS
+  ${MCU_FLAGS}
+)
+
+string(CONCAT CXX_FLAGS
+  ${MCU_FLAGS}
+)
+
+string(CONCAT ASM_FLAGS
+  ${MCU_FLAGS}
+  "-x assembler-with-cpp "
+)
+
+string(CONCAT LINKER_FLAGS
+  "-Wl,--gc-sections "
+  "-static "
+  "-specs=nano.specs "
+)
+
+set(CMAKE_C_FLAGS_INIT ${C_FLAGS})
+set(CMAKE_CXX_FLAGS_INIT ${CXX_FLAGS})
+set(CMAKE_ASM_FLAGS_INIT ${ASM_FLAGS})
+set(CMAKE_EXE_LINKER_FLAGS_INIT ${LINKER_FLAGS})
+set(CMAKE_MODULE_LINKER_FLAGS_INIT ${LINKER_FLAGS})
+set(CMAKE_SHARED_LINKER_FLAGS_INIT ${LINKER_FLAGS})
